b9ee1f810a83d551c061a562fae49c9ecc56cea5
arilieb <ariargoud@gmail.com>
2025-04-25 13:17:18 -0700

    Insertion order set suber implementation.

--- src/keri/db/subing/mod.rs
+++ src/keri/db/subing/mod.rs
@@ -1,6 +1,7 @@
 pub mod cesr;
 pub mod serder;
 pub mod signer;
+pub mod ioset;
 
 use crate::errors::MatterError;
 use crate::keri::db::dbing::BytesDatabase;
@@ -55,6 +56,13 @@ pub trait ValueCodec {
 
 // Default implementation for UTF-8 string serialization/deserialization
 pub struct Utf8Codec;
+
+impl From<std::convert::Infallible> for SuberError {
+    fn from(_: std::convert::Infallible) -> Self {
+        // This code will never run because Infallible can't be created
+        unreachable!("This should never happen as Infallible cannot be instantiated")
+    }
+}
 
 impl ValueCodec for Utf8Codec {
     type Error = SuberError;
